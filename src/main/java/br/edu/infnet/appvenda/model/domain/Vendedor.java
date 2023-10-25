f999489e54a25b99f193a51bbfe0bd4151c6f967
Danilo Antonietto <danilo@grupocfg.com.br>
2023-10-24 22:27:24 -0300

    Aula 24-20-2023

--- src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
+++ src/main/java/br/edu/infnet/appvenda/model/domain/Vendedor.java
@@ -1,6 +1,15 @@
 package br.edu.infnet.appvenda.model.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vendedor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nome;
 	private String cpf;
 	private String email;
